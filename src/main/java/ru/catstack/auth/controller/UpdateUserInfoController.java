0597039ce6ba2952fc574cc3a4a90dde88527d9e
sharphurt <sharphurt@users.noreply.github.com>
2020-04-24 00:09:16 +0500

    - Fixed errors that prevented the application from starting

--- src/main/java/ru/catstack/auth/controller/UpdateUserInfoController.java
+++ src/main/java/ru/catstack/auth/controller/UpdateUserInfoController.java
@@ -23,65 +23,51 @@ public class UpdateUserInfoController {
 
     @PostMapping("/setUsername")
     public ApiResponse setUsername(@Valid @RequestBody SetUsername updateInfo) {
-        var loggedUser = userService.getLoggedInUser();
-        return loggedUser.map(user -> {
-            var newToken = userService.updateUsernameById(user.getId(), updateInfo.getUsername());
-            return new ApiResponse(newToken);
-        }).orElseThrow(() -> new AccessDeniedException("Access denied for unauthenticated user"));
+        var me = userService.getLoggedInUser();
+        var newToken = userService.updateUsernameById(me.getId(), updateInfo.getUsername());
+        return new ApiResponse(newToken);
     }
 
     @PostMapping("/setFirstName")
     public ApiResponse setFirstName(@Valid @RequestBody SetFirstName updateInfo) {
-        var loggedUser = userService.getLoggedInUser();
-        return loggedUser.map(user -> {
-            userService.updateFirstNameById(user.getId(), updateInfo.getFirstName());
-            return new ApiResponse("Information saved successfully");
-        }).orElseThrow(() -> new AccessDeniedException("Access denied for unauthenticated user"));
+        var me = userService.getLoggedInUser();
+        userService.updateFirstNameById(me.getId(), updateInfo.getFirstName());
+        return new ApiResponse("Information saved successfully");
     }
 
     @PostMapping("/setLastName")
     public ApiResponse setLastName(@Valid @RequestBody SetLastName updateInfo) {
-        var loggedUser = userService.getLoggedInUser();
-        return loggedUser.map(user -> {
-            userService.updateLastNameById(user.getId(), updateInfo.getLastName());
-            return new ApiResponse("Information saved successfully");
-        }).orElseThrow(() -> new AccessDeniedException("Access denied for unauthenticated user"));
+        var me = userService.getLoggedInUser();
+        userService.updateLastNameById(me.getId(), updateInfo.getLastName());
+        return new ApiResponse("Information saved successfully");
     }
 
     @PostMapping("/setAge")
     public ApiResponse setAge(@Valid @RequestBody SetAge updateInfo) {
-        var loggedUser = userService.getLoggedInUser();
-        return loggedUser.map(user -> {
-            userService.updateAgeById(user.getId(), updateInfo.getAge());
-            return new ApiResponse("Information saved successfully");
-        }).orElseThrow(() -> new AccessDeniedException("Access denied for unauthenticated user"));
+        var me = userService.getLoggedInUser();
+        userService.updateAgeById(me.getId(), updateInfo.getAge());
+        return new ApiResponse("Information saved successfully");
     }
 
     @PostMapping("/setEmail")
     public ApiResponse setEmail(@Valid @RequestBody SetEmail updateInfo) {
-        var loggedUser = userService.getLoggedInUser();
-        return loggedUser.map(user -> {
-            userService.updateEmailById(user.getId(), updateInfo.getEmail());
-            return new ApiResponse("Information saved successfully");
-        }).orElseThrow(() -> new AccessDeniedException("Access denied for unauthenticated user"));
+        var me = userService.getLoggedInUser();
+        userService.updateEmailById(me.getId(), updateInfo.getEmail());
+        return new ApiResponse("Information saved successfully");
     }
 
     @PostMapping("/setPassword")
     public ApiResponse setPassword(@Valid @RequestBody SetPassword updateInfo) {
-        var loggedUser = userService.getLoggedInUser();
-        return loggedUser.map(user -> {
-            userService.updatePasswordById(user.getId(), updateInfo.getPassword());
-            return new ApiResponse("Information saved successfully");
-        }).orElseThrow(() -> new AccessDeniedException("Access denied for unauthenticated user"));
+        var me = userService.getLoggedInUser();
+        userService.updatePasswordById(me.getId(), updateInfo.getPassword());
+        return new ApiResponse("Information saved successfully");
     }
 
     @PostMapping("/setAbout")
     public ApiResponse setAboutMe(@Valid @RequestBody SetAboutMe updateInfo) {
-        var loggedUser = userService.getLoggedInUser();
-        return loggedUser.map(user -> {
-            userService.updateAboutMeById(user.getId(), updateInfo.getAbout());
-            return new ApiResponse("Information saved successfully");
-        }).orElseThrow(() -> new AccessDeniedException("Access denied for unauthenticated user"));
+        var me = userService.getLoggedInUser();
+        userService.updateAboutMeById(me.getId(), updateInfo.getAbout());
+        return new ApiResponse("Information saved successfully");
     }
 }
 
